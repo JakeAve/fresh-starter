0ebbb00746c4dd2bcc630cf9fc3cac7515684fb0
JakeAve <jacobjavery@gmail.com>
2024-07-30 12:08:52 -0600

    removed unneeded key and id

--- db/passkeyChallengeSchema.ts
+++ db/passkeyChallengeSchema.ts
@@ -2,7 +2,6 @@ import {
   PublicKeyCredentialCreationOptionsJSON,
   PublicKeyCredentialRequestOptionsJSON,
 } from "@simplewebauthn/types";
-import { monotonicUlid } from "$std/ulid/mod.ts";
 import { load } from "$std/dotenv/mod.ts";
 
 const env = await load();
@@ -13,11 +12,8 @@ type ChallengeOptions =
   | PublicKeyCredentialCreationOptionsJSON
   | PublicKeyCredentialRequestOptionsJSON;
 
-export type Challenge = ChallengeOptions & {
-  id: string;
-};
+export type Challenge = ChallengeOptions;
 
-const CHALLENGE_BY_ID: Deno.KvKey = ["passkey_create_options"];
 const CHALLENGE_BY_USER_ID: Deno.KvKey = ["user_passkey_create_options"];
 
 export async function addChallenge(
@@ -26,16 +22,11 @@ export async function addChallenge(
     | PublicKeyCredentialRequestOptionsJSON,
   userId: string,
 ) {
-  const id = monotonicUlid();
+  const challenge: Challenge = { ...creationOptions };
 
-  const challenge: Challenge = { ...creationOptions, id };
-
-  const byId = [...CHALLENGE_BY_ID, challenge.id];
   const byUserId = [...CHALLENGE_BY_USER_ID, userId];
 
   const res = await kv.atomic()
-    .check({ key: byId, versionstamp: null })
-    .set(byId, challenge, { expireIn: 300000 })
     .set(byUserId, challenge, { expireIn: 300000 })
     .commit();
 
