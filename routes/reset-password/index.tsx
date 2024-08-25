9a8cedbd03c5fe366cdaf0a7a927ba45c04d576d
JakeAve <jacobjavery@gmail.com>
2024-08-24 18:55:14 -0600

    styling on password reset page

--- routes/reset-password/index.tsx
+++ routes/reset-password/index.tsx
@@ -87,13 +87,13 @@ export default function Home(props: PageProps<Props>) {
   }
   return (
     <div class="grid place-items-center h-screen">
-      <div class="shadow-md rounded px-8 py-6">
+      <div class="shadow-md rounded px-8 py-6 max-w-sm">
         <p>
           Enter and confirm a new password. You will be redirected to login with
           your new password. If you fail to reset your password in a few
           minutes, you will need to request a new reset code.
         </p>
-        <form class="flex flex-col" method="post">
+        <form class="flex flex-col mt-2" method="post">
           <PasswordPairInputs />
           <Button>Reset Password</Button>
           {message && <p class="text-red-500 mt-2 text-center">{message}</p>}
